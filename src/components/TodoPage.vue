287ed9a610d5ced1c0ec93b9bb76697fee49a5e3
garam park <me@garam.xyz>
2017-10-22 16:27:02 +0900

    5.2.1 삭제

--- src/components/TodoPage.vue
+++ src/components/TodoPage.vue
@@ -8,14 +8,21 @@
     </span>
   </div>
   <ul class="list-group">
-    <li class="list-group-item">
-      청소하기
-      <div class="btn-group pull-right" style="font-size: 12px; line-height: 1;">
-        <button type="button" class="btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <li class="list-group-item" v-for="(todo, index) in todos">
+      {{todo.name}}
+      <div class="btn-group pull-right" 
+        style="font-size: 12px; line-height: 1;">
+        <button type="button" 
+        class="btn-link dropdown-toggle" 
+        data-toggle="dropdown" 
+        aria-haspopup="true" 
+        aria-expanded="false">
           더보기<span class="caret"></span>
         </button>
         <ul class="dropdown-menu">
-          <li><a href="#">삭제</a></li>
+          <li>
+            <a href="#" @click="deleteTodo(index)">삭제</a>
+          </li>
         </ul>
       </div>
     </li>
@@ -24,14 +31,30 @@
 </template>
 
 <script>
-  export default {
-    data(){
-      return {
-        msg:'Example Vue'
-      };
-    },
-    mounted() {
-      console.log('Component mounted.')
+export default {
+  name: 'TodoPage',
+  data () {
+    return {
+      todos: [
+        {
+          name:'청소'
+        },
+        {
+          name:'블로그 쓰기'
+        },
+        {
+          name:'밥먹기'
+        },
+        {
+          name:'안녕'
+        }
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(i){
+      this.todos.splice(i,1);
     }
   }
+}
 </script>
