78c9d584f047a87c55e1c9802aa70f4f3a34b9c4
garam park <me@garam.xyz>
2017-10-22 16:13:43 +0900

    4.3.3 components/Example.vue 추가

---
+++ src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">Panel heading without title</div>
+    <div class="panel-body">
+      Panel content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  data () {
+    return {
+      msg: 'Basic panel example'
+    }
+  }
+}
+</script>
