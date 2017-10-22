ed873a82e53b1f555f8da256634be1b508e27b8e
garam park <me@garam.xyz>
2017-10-22 16:21:39 +0900

    5.2.1 TODO 페이지 - UI 만들기

---
+++ src/components/TodoPage.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="container">
+  <h2>Todo List</h2>
+  <div class="input-group" style="margin-bottom:10px;">
+    <input type="text" class="form-control" placeholder="할일을 입력하세요">
+    <span class="input-group-btn">
+      <button class="btn btn-default" type="button">추가</button>
+    </span>
+  </div>
+  <ul class="list-group">
+    <li class="list-group-item">
+      청소하기
+      <div class="btn-group pull-right" style="font-size: 12px; line-height: 1;">
+        <button type="button" class="btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          더보기<span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a href="#">삭제</a></li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        msg:'Example Vue'
+      };
+    },
+    mounted() {
+      console.log('Component mounted.')
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/example',
       name: 'Example',
       component: Example
+    },
+    {
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 });
