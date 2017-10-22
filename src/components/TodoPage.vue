32c4e9bbcc875a1d49e8810f75e05e6c8c841a76
garam park <me@garam.xyz>
2017-10-22 16:28:57 +0900

    5.2.1 todo 추가

--- src/components/TodoPage.vue
+++ src/components/TodoPage.vue
@@ -2,9 +2,13 @@
 <div class="container">
   <h2>Todo List</h2>
   <div class="input-group" style="margin-bottom:10px;">
-    <input type="text" class="form-control" placeholder="할일을 입력하세요">
+    <input type="text" class="form-control" 
+      placeholder="할일을 입력하세요" 
+      v-model="name" 
+      v-on:keyup.enter="createTodo(name)">
     <span class="input-group-btn">
-      <button class="btn btn-default" type="button">추가</button>
+      <button class="btn btn-default" type="button" 
+      @click="createTodo(name)">추가</button>
     </span>
   </div>
   <ul class="list-group">
@@ -35,6 +39,7 @@ export default {
   name: 'TodoPage',
   data () {
     return {
+      name: null,
       todos: [
         {
           name:'청소'
@@ -54,6 +59,12 @@ export default {
   methods:{
     deleteTodo(i){
       this.todos.splice(i,1);
+    },
+    createTodo(name){
+      if(name != null){
+        this.todos.push({name:name});
+        this.name = null
+      }
     }
   }
 }
